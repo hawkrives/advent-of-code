0bf08b50da55abfc483030d1906085e0d7ec41cb
Hawken Rives <hawkrives@fastmail.fm>
2020-12-09 10:11:00 -0600

    clean up day 9

--- aoc2020/day09/day09p2.py
+++ aoc2020/day09/day09p2.py
@@ -1,4 +1,5 @@
 from itertools import islice, tee, combinations
+from pathlib import Path
 
 def window(iterable, size):
   '''https://stackoverflow.com/a/54110047/2347774'''
@@ -6,15 +7,12 @@ def window(iterable, size):
   iterators = [islice(iterator, i, None) for i, iterator in enumerate(iterators)]  
   yield from zip(*iterators)
 
-#print(list(window(range(26), 25)))
-# [(0, 1, 2), (1, 2, 3), (2, 3, 4)]
-
-sample = [7, 16, 23, 3, 17, 22, 14, 1, 8, 11, 15, 0, 12, 19, 10, 24, 9, 2, 5, 21, 4, 6, 20, 18, 13, 45]
 
 def is_empty(it):
   for _ in it:
     return False
   return True
+
 
 def find_invalid(data, size=25):
   for w in window(data, size+1):
@@ -29,7 +27,6 @@ def find_invalid(data, size=25):
     if is_empty(candidates):
       return n
 
-    #print(n, list(c))
 
 def main(data, size=25):
   invalid = find_invalid(data, size=size)
@@ -40,37 +37,13 @@ def main(data, size=25):
       if sum(w) == invalid:
         return min(w) + max(w)
 
-#main(sample, 25)
 
-sample5 = [
-  35,
-20,
-15,
-25,
-47,
-40,
-62,
-55,
-65,
-95,
-102,
-117,
-150,
-182,
-127,
-219,
-299,
-277,
-309,
-576,
-]
+#file = Path('./sample-25.txt')
+#file = Path('./sample-5.txt')
+file = Path('./data.txt')
 
-#print(main(sample5, 5))
-
-
-with open('./data.txt', 'r') as infile:
+with file.open('r', encoding='utf-8') as infile:
   data = [int(line) for line in infile]
-  #print(data)
   
 print(main(data, 25))
 

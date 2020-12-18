eb4e2a9410b210d0b01dcb396d8b990d9ea421c8
Hawken Rives <rives@stolaf.edu>
2020-12-17 23:18:02 -0600

    clean up day 15

--- aoc2020/day15/day15p2.py
+++ aoc2020/day15/day15p2.py
@@ -1,10 +1,5 @@
-from pathlib import Path
-import collections
-import enum
-import attr
 from contextlib import contextmanager
 import time
-import itertools
 
 
 @contextmanager
@@ -15,45 +10,17 @@ def timed(label='Runtime'):
   print(f"{label}: {end - start:.3f}s")
 
 
-def parse(datafile):
-  with open(datafile, 'r', encoding='utf-8') as infile:
-    for line in infile:
-      line = line.strip()
-
-      if not line:
-        continue
-
-      yield line
-
-
-def load():
-  datafile = Path('./sample.txt')
-  #datafile = Path('./data.txt')
-  #datafile = Path('./data-drew.txt')
-
-  yield from parse(datafile)
-
-
-def main():
-  data = load()
-  
-  ...
-  
-
-def evaluate(starting):
+def evaluate(starting, *, iterations: int):
   history = {v: [turn + 1] for turn, v in enumerate(starting)}
 
   last_spoken = starting[-1]
-  for turn in range(len(starting) + 1, 30_000_000 + 1):
+  for turn in range(len(starting) + 1, iterations + 1):
     if turn % 1_000_000 == 0:
       print(f'{turn:,}')
+
     # we need to know the number of times last_spoken has been said
     # and we need to know the most recent two turns on which the it was said
-    turns_spoken = history.setdefault(last_spoken, [])
-
-    #print(history)
-    #print(turns_spoken)
-    #print(last_spoken)
+    turns_spoken = history.get(last_spoken, [])
 
     if len(turns_spoken) < 2:
       value = 0
@@ -63,9 +30,6 @@ def evaluate(starting):
     last_spoken = value
     history.setdefault(last_spoken, []).append(turn)
     
-    #print('said', last_spoken)
-    #print()
-
   return last_spoken
 
 
@@ -91,5 +55,4 @@ if __name__ == '__main__':
 
   input = (15,5,1,4,7,0)
   with timed(str(input)):
-    print(evaluate(input))
-
+    print(evaluate(input, iterations=30_000_000))

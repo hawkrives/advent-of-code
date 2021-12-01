d6fb12266f160e9d2559406569c1bdadd13ca4b9
Hawken MacKay Rives <hawken.rives@techcyte.com>
2021-12-01 12:24:27 -0500

    2021/01: update controller

--- 2021/01/rust/src/main.rs
+++ 2021/01/rust/src/main.rs
@@ -6,10 +6,15 @@ const EXAMPLE_DATA: &'static str = include_str!("../input.example.txt");
 
 fn main() {
     println!("Part 1:");
-    println!("Sample: {}", p01::analyze_measurements(EXAMPLE_DATA));
-    println!("Actual: {}", p01::analyze_measurements(INPUT_DATA));
+    let sample_1 = p01::analyze_measurements(EXAMPLE_DATA);
+    println!("Sample: expected {}, got {}", 7, sample_1);
+    let actual_1 = p01::analyze_measurements(INPUT_DATA);
+    println!("Actual: expected {}, got {}", 1692, actual_1);
     println!();
+
     println!("Part 2:");
-    println!("Sample: {}", p02::analyze_measurements(EXAMPLE_DATA));
-    println!("Actual: {}", p02::analyze_measurements(INPUT_DATA));
+    let sample_2 = p02::analyze_measurements(EXAMPLE_DATA);
+    println!("Sample: expected {}, got {}", 5, sample_2);
+    let actual_2 = p02::analyze_measurements(INPUT_DATA);
+    println!("Actual: expected {}, got {}", 1724, actual_2);
 }

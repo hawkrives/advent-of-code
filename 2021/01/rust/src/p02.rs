e10db5f362809d2576031086b3bd993c9b044bfb
Hawken MacKay Rives <hawken.rives@techcyte.com>
2021-12-01 12:24:15 -0500

    2021/01/p02: rework to use Itertools::tuple_windows

--- 2021/01/rust/src/p02.rs
+++ 2021/01/rust/src/p02.rs
@@ -1,18 +1,16 @@
+use itertools::Itertools;
+
 pub fn analyze_measurements(measurements: &str) -> usize {
-    let measurements = measurements
+    measurements
         .split_ascii_whitespace()
         .filter_map(|m| m.parse::<u64>().ok())
-        .collect::<Vec<_>>();
-
-    let m2 = measurements
-        .windows(3)
-        .map(|set| set.iter().sum::<u64>())
-        .collect::<Vec<_>>();
-
-    m2.windows(2)
-        .filter(|pair| match pair {
-            [a, b] => a < b,
-            _ => panic!("expected a two-element array"),
-        })
+        // group into 3-tuples
+        .tuple_windows::<(_, _, _)>()
+        // and sum each 3-tuple
+        .map(|(a, b, c)| a + b + c)
+        // now group those summed 3-tuples into 2-tuples,
+        .tuple_windows::<(_, _)>()
+        // and count only the ones that directly increase in size
+        .filter(|(a, b)| a < b)
         .count()
 }

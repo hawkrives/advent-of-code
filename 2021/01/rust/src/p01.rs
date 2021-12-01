937da219e521823b6cf49bf4a17f3aba5b983c8f
Hawken MacKay Rives <hawken.rives@techcyte.com>
2021-12-01 12:23:40 -0500

    2021/01: rework both parts to use tuple.windows()

--- 2021/01/rust/src/p01.rs
+++ 2021/01/rust/src/p01.rs
@@ -1,19 +1,14 @@
-pub fn analyze_measurements(measurements: &str) -> u64 {
+pub fn analyze_measurements(measurements: &str) -> usize {
     let measurements = measurements
         .split_ascii_whitespace()
         .filter_map(|m| m.parse::<u64>().ok())
         .collect::<Vec<_>>();
 
-    let (first, remaining) = measurements.split_first().expect("no measurements found?");
-
-    let mut prior = first;
-    let mut increase_count: u64 = 0;
-    for depth in remaining {
-        if let std::cmp::Ordering::Greater = depth.cmp(prior) {
-            increase_count += 1;
-        }
-        prior = depth;
-    }
-
-    return increase_count;
+    measurements
+        .windows(2)
+        .filter(|pair| match pair {
+            [a, b] => a < b,
+            _ => panic!("expected a two-element array"),
+        })
+        .count()
 }
--- 2021/01/rust/src/p02.rs
+++ 2021/01/rust/src/p02.rs
@@ -1,31 +1,18 @@
-use itertools::izip;
-
-pub fn analyze_measurements(measurements: &str) -> u64 {
+pub fn analyze_measurements(measurements: &str) -> usize {
     let measurements = measurements
         .split_ascii_whitespace()
         .filter_map(|m| m.parse::<u64>().ok())
         .collect::<Vec<_>>();
 
-    let offset0 = measurements.iter();
-    let mut offset1 = measurements.iter();
-    offset1.next();
-    let mut offset2 = measurements.iter();
-    offset2.next();
-    offset2.next();
+    let m2 = measurements
+        .windows(3)
+        .map(|set| set.iter().sum::<u64>())
+        .collect::<Vec<_>>();
 
-    let mut combined = izip!(offset0, offset1, offset2);
-
-    let first = combined.next().expect("no measurements found?");
-
-    let mut prior = first.0 + first.1 + first.2;
-    let mut increase_count: u64 = 0;
-    for depth in combined {
-        let summed_depth = depth.0 + depth.1 + depth.2;
-        if let std::cmp::Ordering::Greater = summed_depth.cmp(&prior) {
-            increase_count += 1;
-        }
-        prior = summed_depth;
-    }
-
-    return increase_count;
+    m2.windows(2)
+        .filter(|pair| match pair {
+            [a, b] => a < b,
+            _ => panic!("expected a two-element array"),
+        })
+        .count()
 }
